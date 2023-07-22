9102107122e8896f535c5ac07cf33b9ba1b4ae58
Camilo Abarca <cabarcaz@yahoo.cl>
2023-07-21 21:31:49 -0400

    se agrega exception base

---
+++ src/modules/user/domain/exceptions/user.exception.ts
@@ -0,0 +1,33 @@
+import { DomainException, DomainExceptionCode } from './domain.exception';
+
+export class UserNameRequiredException extends DomainException{
+	constructor(){
+		super(UserNameRequiredException.getMessage())
+		this.name = DomainExceptionCode.USER_NAME_REQUIRED
+	}
+	static getMessage(){
+		return 'Name is required'
+	}
+}
+
+export class UserLastRequiredException extends DomainException{
+	constructor(){
+		super(UserLastRequiredException.getMessage())
+		this.name = DomainExceptionCode.USER_LASTNAME_REQUIRED
+	}
+	static getMessage(){
+		return 'Lastname is required'
+	}
+}
+
+export class UserEmailRequiredException extends DomainException{
+	constructor(){
+		super(UserEmailRequiredException.getMessage())
+		this.name = DomainExceptionCode.USER_EMAIL_REQUIRED
+	}
+	static getMessage(){
+		return 'Email is required'
+	}
+}
+
+
