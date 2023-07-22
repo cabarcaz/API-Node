bedf4a4fdfd6a731feeff6ce10d1c9e3e438cdff
Camilo Abarca <cabarcaz@yahoo.cl>
2023-07-21 22:11:49 -0400

    User Entity + insert user

---
+++ src/modules/user/application/user.application.ts
@@ -0,0 +1,13 @@
+import User from '../domain/user';
+import { UserRepository } from '../domain/user.repository';
+
+export default class UserApplication{
+
+	//Solid Principle: Inversion dependency
+	//Desing Pattern: Injection dependency
+	constructor(private readonly userRepository: UserRepository){}
+
+	insert(user: User){
+		return this.userRepository.insert(user)
+	}
+}
--- src/modules/user/infraestructure/user.infrastructure.ts
+++ src/modules/user/infraestructure/user.infrastructure.ts
@@ -1,5 +1,7 @@
+import databaseBootstrap from '../../../bootstrap/database.bootstrap';
 import User from '../domain/user';
 import { UserRepository } from '../domain/user.repository';
+import { UserEntity } from './user.entity';
 
 export default class UserInfraestructure implements UserRepository{
 	list(): Promise<User[]> {
@@ -8,9 +10,25 @@ export default class UserInfraestructure implements UserRepository{
 	listOne(guid: string): Promise<User> {
 		throw new Error('Method not implemented.');
 	}
-	insert(user: User): Promise<User> {
-		throw new Error('Method not implemented.');
+
+	async insert(user: User): Promise<User> {
+		const userInsert = new UserEntity()
+		const { guid, name, lastname, email, password, refreshToken, active } = user.propierties()
+		Object.assign(userInsert, {
+			guid,
+			name,
+			lastname,
+			email: email.value,
+			password,
+			refreshToken,
+			active
+		})
+
+		await databaseBootstrap.dataSource.getRepository(UserEntity).save(userInsert)
+		return user
 	}
+
+
 	update(user: User): Promise<User> {
 		throw new Error('Method not implemented.');
 	}
