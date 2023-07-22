feec83538b4b7ef7c42c957d99a5545dec38c5f6
Camilo Abarca <cabarcaz@yahoo.cl>
2023-07-21 22:23:49 -0400

    user list

--- src/modules/user/infraestructure/user.infrastructure.ts
+++ src/modules/user/infraestructure/user.infrastructure.ts
@@ -1,12 +1,30 @@
 import databaseBootstrap from '../../../bootstrap/database.bootstrap';
 import User from '../domain/user';
 import { UserRepository } from '../domain/user.repository';
+import { EmailVO } from '../domain/value-objects/email.VO';
 import { UserEntity } from './user.entity';
 
 export default class UserInfraestructure implements UserRepository{
-	list(): Promise<User[]> {
-		throw new Error('Method not implemented.');
+	async list(): Promise<User[]> {
+		const repo = databaseBootstrap.dataSource.getRepository(UserEntity)
+		const result = await repo.find({ where: { active: true } })
+
+		return result.map((el: UserEntity) => {
+			const emailResult = EmailVO.create(el.email)
+			// TODO: pendiente neverthow
+			return new User({
+				guid: el.guid,
+				name: el.name,
+				lastname: el.lastname,
+				email: emailResult.value,
+				password: el.password,
+				refreshToken: el.refreshToken,
+				active: el.active
+			})
+		})
 	}
+
+
 	listOne(guid: string): Promise<User> {
 		throw new Error('Method not implemented.');
 	}
