a7fad56f8c7e65c6b713269ec116ed4b7da03b0e
Camilo Abarca <cabarcaz@yahoo.cl>
2023-07-22 23:58:47 -0400

    User controller + dtos

---
+++ src/modules/user/interfaces/http/dto/user-update.dto.ts
@@ -0,0 +1,23 @@
+import { UserProperties } from 'src/modules/user/domain/user';
+import { DTO } from './dto.generic';
+
+interface UserDTO{
+	name: string,
+	lastname: string,
+	email: string,
+	guid: string
+}
+
+export type UserUpdateDTO = UserDTO
+
+export class UserUpdateMapping extends DTO<UserProperties, UserUpdateDTO>{
+	execute(data: UserProperties): UserUpdateDTO {
+		return{
+			name: data.name,
+			lastname: data.lastname,
+			email: data.email.value,
+			guid: data.guid
+		}
+	}
+
+}
--- src/modules/user/interfaces/http/user.controller.ts
+++ src/modules/user/interfaces/http/user.controller.ts
@@ -4,6 +4,11 @@ import { IError } from '../helper/ierror';
 import UserFactory from '../../domain/user-factory';
 import UserApplication from '../../application/user.application';
 import { UserInsertMapping } from './dto/user-insert.dto';
+import { UserListMapping } from './dto/user-list.dto';
+import { GuidVO } from '../../domain/value-objects/guid.vo';
+import { UserListOneMapping } from './dto/user-list-one.dto';
+import { UserUpdateMapping } from './dto/user-update.dto';
+import { UserDeleteMapping } from './dto/user-delete.dto';
 
 export  default class {
 	constructor(private application: UserApplication){
@@ -30,19 +35,72 @@ export  default class {
 			const result = new UserInsertMapping().execute(data.propierties())
 			res.status(201).json(result)
 		}
-
 	}
 
-	async list(req: Request, res: Response){
+	async list(_req: Request, res: Response){
 		const list = await this.application.list()
-		const result =
+		const result = new UserListMapping().execute(list.map(user => user.propierties()))
+		res.json(result)
 	}
 
-	async listOne(req: Request, res: Response){}
+	async listOne(req: Request, res: Response, next: NextFunction){
+		const { guid } = req.params
+		const guidResult = GuidVO.create(guid)
+		if(guidResult.isErr()){
+			const err: IError = new Error(guidResult.error.message)
+			err.status = 411
+			return next(err)
+		}
 
-	async update(req: Request, res: Response){}
+		const userResult = await this.application.listOne(guid)
+		if(userResult.isErr()){
+			return res.status(404).send(userResult.error.message)
+		}else if(userResult.isOk()){
+			const result = new UserListOneMapping().execute(userResult.value.propierties())
+			res.json(result)
+		}
+	}
 
-	async delete(req: Request, res: Response){}
+	async update(req: Request, res: Response, next: NextFunction){
+		const { guid } = req.params
+		const fieldsToUpdate = req.body
+		const guidResult = GuidVO.create(guid)
+		if(guidResult.isErr()){
+			const err: IError = new Error(guidResult.error.message)
+			err.status = 411
+			return next(err)
+		}
+
+		const dataResult = await this.application.update(guid, fieldsToUpdate)
+		if(dataResult.isErr()){
+			const err: IError = new Error(dataResult.error.message)
+			err.status = 411
+			return next(err)
+		}else{
+			const result = new UserUpdateMapping().execute(dataResult.value.propierties())
+			res.json(result)
+		}
+
+	}
+
+	async delete(req: Request, res: Response, next: NextFunction){
+		const { guid } = req.params
+		const guidResult = GuidVO.create(guid)
+		if(guidResult.isErr()){
+			const err: IError = new Error(guidResult.error.message)
+			err.status= 411
+			return next(err)
+		}
+		const dataResult = await this.application.delete(guid)
+		if(dataResult.isErr()){
+			const err: IError = new Error(dataResult.error.message)
+			err.status = 404
+			return next(err)
+		}else{
+			const result = new UserDeleteMapping().execute(dataResult.value.propierties())
+			res.json(result)
+		}
+	}
 
 
 
