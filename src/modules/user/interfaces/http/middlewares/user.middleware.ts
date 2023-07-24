8b9e4d17472235755ecf0c0e98baff39e26e7613
Camilo Abarca <cabarcaz@yahoo.cl>
2023-07-23 22:34:54 -0400

    middleware - decorators

---
+++ src/modules/user/interfaces/http/middlewares/user.middleware.ts
@@ -0,0 +1,24 @@
+import { validate } from 'class-validator';
+import { NextFunction, Response, Request } from "express";
+import { UserListOneValidator } from "../validators/userListOne.validator";
+
+class UserMiddleware {
+
+	static async ValidateListOne(req: Request, res: Response, next: NextFunction) {
+		const { guid } = req.params
+		const userListOneValidator = new UserListOneValidator()
+		userListOneValidator.guid = guid
+		const errors = await validate(userListOneValidator)
+
+		if (errors.length > 0) {
+			console.log(errors)
+			return next(new Error('Invalid request'))
+		}
+
+		next()
+	}
+}
+
+export const MiddlewareListOne: ((req: Request, res: Response, next: NextFunction) => Promise<void>)[] = [
+	UserMiddleware.ValidateListOne
+]
--- src/modules/user/interfaces/http/user.routes.ts
+++ src/modules/user/interfaces/http/user.routes.ts
@@ -3,6 +3,7 @@ import { UserRepository } from '../../domain/user.repository';
 import UserInfraestructure from '../../infraestructure/user.infrastructure';
 import UserApplication from '../../application/user.application';
 import UserController from './user.controller';
+import { MiddlewareListOne } from './middlewares/user.middleware';
 
 
 const infrastructure: UserRepository = new UserInfraestructure()
@@ -19,11 +20,11 @@ class UserRouter {
 
 	mountRoutes() {
 		// Desing pattern: Chains of responsability
-		this.expressRouter.post('/', controller.insert)
-		this.expressRouter.get('/', controller.list)
-		this.expressRouter.get('/:guid', controller.listOne)
-		this.expressRouter.put('/:guid', controller.update)
-		this.expressRouter.delete('/:guid', controller.delete)
+		this.expressRouter.post('/insert', controller.insert)
+		this.expressRouter.get('/list', controller.list)
+		this.expressRouter.get('/listOne/:guid', ...MiddlewareListOne, controller.listOne)
+		this.expressRouter.put('/update/:guid', controller.update)
+		this.expressRouter.delete('/delete/:guid', controller.delete)
 	}
 
 }
